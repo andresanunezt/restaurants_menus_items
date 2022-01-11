18533201b89934599e60ff167367be308f0d431c
andresanunezt <andresanunez98@gmail.com>
2022-01-11 15:07:39 -0500

    first commit

---
+++ models/Restaurant.js
@@ -0,0 +1,21 @@
+const {sequelize, DataTypes, Model} = require('./_index');
+
+/**
+ * Represents a Restaurant
+ */
+class Restaurant extends Model {
+
+    // add methods here
+
+}
+Restaurant.init({
+    name: DataTypes.STRING,
+    image: DataTypes.STRING,
+}, {
+    sequelize,
+    timestamps: false,
+});
+
+module.exports = {
+    Restaurant
+};
